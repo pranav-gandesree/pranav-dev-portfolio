ba03135d32e14175fa9a7a45ccb0bd6ede5b1b8f
pranav <pranav8267@gmail.com>
2025-03-21 19:15:59 +0530

    update content

--- components/canvas/Header.tsx
+++ components/canvas/Header.tsx
@@ -3,11 +3,10 @@ import Socials from "./Socials";
 const Header = () => (
   <header className="mb-16">
     <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
-      Hello, I&apos;m Pranav Gandesree.
+      Hello, I&apos;m Pranav
     </h1>
     <p className="text-lg md:text-xl text-gray-400 mb-8">
-      21-year-old passionate tech enthusiast on a journey to create cool projects and make my mark in the world of tech! Exploring the latest in Web3, blockchain and decentralized
-      technologies. 
+      Full stack developer exploring solana blockchain.
     </p>
     <Socials />
   </header>
--- components/sections/AboutMe.tsx
+++ components/sections/AboutMe.tsx
@@ -6,7 +6,7 @@ const AboutMe = () => (
     <h2 className="text-2xl font-bold mb-4">About Me</h2>
 
     <p className="text-gray-400 mb-10 text-lg">
-      I&apos;m a 21-year-old tech enthusiast with a degree in Computer Science
+      I&apos;m a 22-year-old tech enthusiast with a degree in Computer Science
       and Engineering, specializing in IoT. I love solving problems through
       technology and creating user-friendly, impactful web applications.
     </p>
